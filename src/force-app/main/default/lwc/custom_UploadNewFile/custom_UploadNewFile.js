ada87df3c02e3ca02475a980faac869895ddba63
Adam Tavares <tavares.nip@gmail.com>
2022-02-14 11:52:08 -0300

    [14-02-2022] - For:each progress updated and hide step

--- src/force-app/main/default/lwc/custom_UploadNewFile/custom_UploadNewFile.js
+++ src/force-app/main/default/lwc/custom_UploadNewFile/custom_UploadNewFile.js
@@ -1,10 +1,16 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class Custom_UploadNewFile extends LightningElement {
+    @track uploadSteps = [
+        {label: 'Select file', value: 0, show: true},
+        {label: 'Validate file', value: 1,show: true},
+        {label: 'Converting CSV File', value: 2, show: true},
+        {label: 'Upload file', value: 3, show: true},
 
+    ]
     @api
     myRecordId;
-
+    @track currentStep = 0;
     get acceptedFormats() {
         return ['.pdf', '.png'];
     }
@@ -13,5 +19,16 @@ export default class Custom_UploadNewFile extends LightningElement {
         // Get the list of uploaded files
         const uploadedFiles = event.detail.files;
         alert('No. of files uploaded : ' + uploadedFiles.length);
+    }
+
+    toggleStep4(){
+        console.log(this.currentStep);
+        arr.splice(2, 0, "Lene");
+        this.currentStep++;
+    }
+
+    hideCsv(){
+        console.log(this.currentStep);
+        this.uploadSteps[2].show = !this.uploadSteps[2].show;
     }
 }
